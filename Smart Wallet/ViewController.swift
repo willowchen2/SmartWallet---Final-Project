5cbfda6ed45c9b7545949cb4c12c9cc562f74830
Willow Chen <willow.chen425@gmail.com>
2022-07-19 14:14:40 -0700

    Initial Commit

---
+++ Smart Wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Wallet
+//
+//  Created by user1 on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
