d34da0ca8d8997896dd00b19aa02f0633e3a2407
6ecilia <cecilia.h.qian@gmail.com>
2022-07-21 10:54:06 -0700

    budget 2

--- Smart Wallet/budgetViewController.swift
+++ Smart Wallet/budgetViewController.swift
@@ -8,29 +8,23 @@
 import UIKit
 
 class budgetViewController: UIViewController {
+    
     @IBOutlet weak var question: UILabel!
     
-    @IBOutlet weak var textField: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    
+    
     }
-    @IBAction func submitButton(_ sender: UIButton) {
+    
+   
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
         if let newTitle = textField.text {
             question.text = newTitle
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
