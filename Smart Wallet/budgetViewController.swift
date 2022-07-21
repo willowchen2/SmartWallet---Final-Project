e3c954ecac6595d000cfbf4b8db579101a28da64
6ecilia <cecilia.h.qian@gmail.com>
2022-07-21 12:18:33 -0700

    b

--- Smart Wallet/budgetViewController.swift
+++ Smart Wallet/budgetViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
+var mathcal = Double()
+let expensee = Double()
+let transportt = Double()
+let foodd = Double()
+let clothingg = Double()
+let selfcaree = Double()
+let phonee = Double()
+let rentt = Double()
+let savingss = Double()
+let otherr = Double()
+
 class budgetViewController: UIViewController {
     
     @IBOutlet weak var question: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var submitButon: UIButton!
+    
+    @IBOutlet var result: UIView!
+    @IBOutlet weak var expense: UITextField!
+    @IBOutlet weak var transport: UITextField!
+    @IBOutlet weak var food: UITextField!
+    @IBOutlet weak var clothing: UITextField!
+    @IBOutlet weak var selfcare: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var rent: UITextField!
+    @IBOutlet weak var savings: UITextField!
+    @IBOutlet weak var other: UITextField!
+    @IBAction func calculate(_ sender: Any) {
+        
+    
+        
+    }
+    
+    
+    
     
     override func viewDidLoad() {
         question.isHidden = false
