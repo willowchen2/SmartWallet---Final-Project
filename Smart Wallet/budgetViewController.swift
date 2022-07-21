421cc8005a86133bce33f77b7efc54fc632326b1
6ecilia <cecilia.h.qian@gmail.com>
2022-07-21 11:02:46 -0700

    budget 5

--- Smart Wallet/budgetViewController.swift
+++ Smart Wallet/budgetViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 class budgetViewController: UIViewController {
     
     @IBOutlet weak var question: UILabel!
-    
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
+        question.isHidden = false
+        textField.isHidden = false
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -25,6 +26,10 @@ class budgetViewController: UIViewController {
         if let newTitle = textField.text {
             question.text = newTitle
         }
+    }
+    @IBAction func submitButton(_ sender: Any) {
+        question.isHidden = true
+        question.isHidden = true
     }
     
 }
