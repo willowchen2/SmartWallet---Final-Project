e7a5a6d9b0dbac50fc5c0997390f18666542392e
Christina <christinayao698@gmail.com>
2022-07-21 14:40:00 -0700

    potential purchases!!

--- Smart Wallet/ViewController.swift
+++ Smart Wallet/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Smart Wallet
 //
-//  Created by user1 on 7/19/22.
+//  Created by Christina Yao on 7/21/22.
 //
 
 import UIKit
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
