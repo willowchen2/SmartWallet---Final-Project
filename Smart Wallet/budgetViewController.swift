ac63211a84e9eb1843a4471c697585de7528a875
6ecilia <cecilia.h.qian@gmail.com>
2022-07-21 15:13:45 -0700

    budget draft!!!

--- Smart Wallet/budgetViewController.swift
+++ Smart Wallet/budgetViewController.swift
@@ -23,6 +23,8 @@ class budgetViewController: UIViewController {
     @IBOutlet weak var question: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var submitButon: UIButton!
+    @IBOutlet weak var textFieldd: UILabel!
+    
     
     @IBOutlet var result: UIView!
     @IBOutlet weak var expense: UITextField!
@@ -35,9 +37,30 @@ class budgetViewController: UIViewController {
     @IBOutlet weak var savings: UITextField!
     @IBOutlet weak var other: UITextField!
     @IBAction func calculate(_ sender: Any) {
+   
+        let expensee = Double(expense.text!)
+        let transportt = Double(transport.text!)
+        let foodd = Double(food.text!)
+        let clothingg = Double(clothing.text!)
+        let selfcaree = Double(selfcare.text!)
+        let phonee = Double(phone.text!)
+        let rentt = Double(rent.text!)
+        let savingss = Double(savings.text!)
+        let otherr = Double(other.text!)
         
-    
+        mathcal = expensee! + transportt! + foodd! + clothingg! + selfcaree! + phonee! + rentt! + savingss! + otherr!
+        let Answer = String(format: "%.2f", arguments: [mathcal])
+        let Firstvalue = String(format: "%.2f", arguments: [expensee!])
+        let Secondvalue = String(format: "%.2f", arguments: [transportt!])
+        let Thirdvalue = String(format: "%.2f", arguments: [foodd!])
+        let Fourthvalue = String(format: "%.2f", arguments: [clothingg!])
+        let Fifthvalue = String(format: "%.2f", arguments: [selfcaree!])
+        let Sixthvalue = String(format: "%.2f", arguments: [phonee!])
+        let Seventhvalue = String(format: "%.2f", arguments: [rentt!])
+        let Eighthvalue = String(format: "%.2f", arguments: [savingss!])
+        let Ninthvalue = String(format: "%.2f", arguments: [otherr!])
         
+        textFieldd.text = "\(Firstvalue) + \(Secondvalue) + \(Thirdvalue) + \(Fourthvalue) + \(Fifthvalue) + \(Sixthvalue) + \(Seventhvalue) + \(Eighthvalue) + \(Ninthvalue) = \(Answer)"
     }
     
     
